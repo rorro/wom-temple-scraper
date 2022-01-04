5a883c61834f3bbed9b72436abc0fb9d60d3c1b2
dekvall <dkvldev@gmail.com>
2022-01-04 20:51:58 +0100

    correct filepaths

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 . .venv/bin/activate
-python rate-dumper.py ehp ~/personal_projects/wise-old-man/server/src/api/modules/efficiency/configs/ehp
-python rate-dumper.py ehb ~/personal_projects/wise-old-man/server/src/api/modules/efficiency/configs/ehb
-pushd ~/personal_projects/wise-old-man/server/src/api/modules/efficiency/configs
+python rate-dumper.py ehp ~/personal-projects/wise-old-man/server/src/api/modules/efficiency/configs/ehp
+python rate-dumper.py ehb ~/personal-projects/wise-old-man/server/src/api/modules/efficiency/configs/ehb
+pushd ~/personal-projects/wise-old-man/server/src/api/modules/efficiency/configs
 npx prettier --write **/*.ts
 popd
 deactivate
